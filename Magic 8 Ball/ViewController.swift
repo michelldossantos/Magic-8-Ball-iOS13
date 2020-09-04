f91306026f2556d99a95eebab06e11069d497497
Michel dos Santos <michelsantos15@gmail.com>
2020-09-04 07:02:33 -0300

    Aprendendo a trabalhar com Branches(Teste)

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
+    // teste para trabalhar com branches
 
 
 }
